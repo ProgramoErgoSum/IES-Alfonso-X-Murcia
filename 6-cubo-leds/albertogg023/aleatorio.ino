7449803ec5467d60a9b3aea8e7856c3571515917
Alberto Gálvez <37451024+albertogg023@users.noreply.github.com>
2018-06-20 10:02:24 +0200

    Create aleatorio.ino

---
+++ 6-cubo-leds/albertogg023/aleatorio.ino
@@ -0,0 +1,39 @@
+#Aleatorio
+
+```
+void setup() {
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  
+}
+
+void apagar_todo(){
+  for (int x=0; x<9; x++){
+   digitalWrite(x,0);
+  }
+}
+
+int randNumberX = 0, randNumberY = 0;
+
+void loop(){
+  
+   randNumberX = random(0,5);
+   randNumberY = random(5,9);
+ 
+  digitalWrite(randNumberX,HIGH); 
+  digitalWrite(randNumberY,HIGH);
+  
+   delay(100);
+   
+  digitalWrite(randNumberX,LOW; 
+  digitalWrite(randNumberY,LOW);
+  
+}
+```
